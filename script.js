8e892f51d9f518a96b0ababed1856e77b3a3c8b5
Glitch (alive-hisser) <none>
2018-03-30 18:29:59 +0000

    🤒💫 Checkpoint ./index.html:136798/1570 ./script.js:136798/1255 ./package.json:136798/102

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hi');
+// import accessibleAutocomplete from 'accessible-autocomplete'
+
+window.accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker')
+})
+
+const defaultCommands = [
+  'France',
+  'Germany',
+  'United Kingdom'
+]
+
+function suggest (query, populateResults) {
+  const filteredResults = defaultCommands.filter(result => result.indexOf(query) !== -1)
+  console.log(filteredResults)
+  populateResults(filteredResults)
+}
+
+window.accessibleAutocomplete({
+  element: document.querySelector('#my-autocomplete-container'),
+  id: 'my-autocomplete', // To match it to the existing <label>.
+  // source: defaultCommands
+  source: suggest,
+  showAllValues: true
+})
