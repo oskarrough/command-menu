356c719e6f891758c8ab226960fb0fe092adc3ec
Glitch (autocomplete-adventures) <none>
2018-03-30 20:47:11 +0000

    🌕🎉 Checkpoint ./script.js:136798/1261

--- script.js
+++ script.js
@@ -4,12 +4,6 @@
 //   selectElement: document.querySelector('#location-picker')
 // })
 
-const countries = [
-  'France',
-  'Germany',
-  'United Kingdom'
-]
-
 const commands = [
 	{
 		label: "open something",
@@ -25,24 +19,22 @@ const commands = [
 	}
 ]
 
-const x = commands.map(c => c.label)
-
 function suggest(query, populateResults) {
-  const results = window.fuzzysort.go(query, commands, {key: 'label'})
-  console.log(results)
-  // const filteredResults = commands.filter(result => {
-  //   return result.label.indexOf(query) !== -1
-  // })
-  // const labels = filteredResults.map(result => result.label)
-  // console.log({filteredResults})
-  // populateResults(filteredResults)
-  // const filtered = commands.filter(c => c.label
-  populateResults(results)
+  let suggestions = commands
+  
+  const searchResults = window.fuzzysort.go(query, commands, {key: 'label'})
+  if (searchResults.total > 0) {
+    suggestions = searchResults.map(r => r.obj)
+  }
+  
+  console.log({suggestions})
+  populateResults(suggestions)
 }
 
 function inputValueTemplate(value) {
-  console.log({value})
-  return "test"
+  // console.log({value})
+  if (value) return value.label
+  return value
 }
 
 function suggestionTemplate (suggestion) {
@@ -52,11 +44,10 @@ function suggestionTemplate (suggestion) {
 window.accessibleAutocomplete({
   element: document.querySelector('#my-autocomplete-container'),
   id: 'my-autocomplete', // To match it to the existing <label>.
-  // source: countries,
   source: suggest,
   showAllValues: true,
   templates: {
-    // inputValue: inputValueTemplate,
+    inputValue: inputValueTemplate,
     suggestion: suggestionTemplate
   },
   onConfirm: function(confirmed) {
