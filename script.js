53ba48f1e380cda4a215130b73c741678c8511c0
Glitch (autocomplete-adventures) <none>
2018-03-30 20:57:32 +0000

    🍓🐁 Checkpoint ./script.js:136798/3689 ./styles.css:136798/98 ./index.html:136798/326

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-// import accessibleAutocomplete from 'accessible-autocomplete'
-
-// window.accessibleAutocomplete.enhanceSelectElement({
-//   selectElement: document.querySelector('#location-picker')
-// })
+// yarn add accessible-autocomplete fuzzysort
 
 const commands = [
-	{
-		label: "open something",
-		shortcut: 'Shift+O'
-	},
-	{
-		label: "new tab",
-		shortcut: 'CMD+N'
-	},
-	{
-		label: "new window",
-    shortcut: 'CMD+Shift+O'
-	}
+  {keys: 'p', label: 'play/pause the session'},
+  {keys: 'n', label: 'play next track in current radio'},
+  {keys: 's', label: 'shuffle current track selection'},
+  {keys: 'm', label: '(un)mute the volume'},
+  {keys: 'r', label: 'play a random radio channel'},
+  {keys: 'f', label: 'cycle through formats (default, fullscreen, minimized)'},
+  {keys: 'g h', label: 'go to home'},
+  {keys: 'g r', label: 'go to all radios'},
+  {keys: 'g m', label: 'go to map'},
+  {keys: 'g y', label: 'go to history (y, as in your web-browser)'},
+  {keys: 'g i', label: 'go to my radio (i, as in I, me)'},
+  {keys: 'g s', label: 'go to my favorite radios (s, as in starred)'},
+  {keys: 'g t', label: 'go to my tracks'},
+  {keys: 'g a', label: 'go to add'},
+  {keys: 'g f', label: 'go to feedback'},
+  {keys: 'g c', label: 'go to current radio (the one being played)'},
+  {keys: 'g x', label: 'go to the track being played (x, as in a cross to locate the track/trax)'}
 ]
 
 function suggest(query, populateResults) {
@@ -38,7 +39,7 @@ function inputValueTemplate(value) {
 }
 
 function suggestionTemplate (suggestion) {
-  return `<b>${suggestion.label}</b> <kbd>${suggestion.shortcut}</kbd>`
+  return `${suggestion.label} <kbd>${suggestion.keys}</kbd>`
 }
 
 window.accessibleAutocomplete({
@@ -52,5 +53,7 @@ window.accessibleAutocomplete({
   },
   onConfirm: function(confirmed) {
     console.log({confirmed})
+    let el = document.querySelector('#my-autocomplete-confirmed')
+    el.textContent = `You chose ${suggestionTemplate(confirmed)}`
   }
 })
