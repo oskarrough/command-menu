04da230c9a0ca3f75f92e1f17618d3f760664732
Glitch (autocomplete-adventures) <none>
2018-03-31 18:29:54 +0000

    👨🕍 Checkpoint ./script.js:136798/412 ./index.html:136798/133

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ window.accessibleAutocomplete({
   element: document.querySelector('#my-autocomplete-container'),
   id: 'my-autocomplete', // To match it to the existing <label>.
   source: suggest,
+  placeholder: 'Search for a command',
   autoselect: true,
   confirmOnBlur: true, // should be true for touch at least
   showAllValues: true,
@@ -55,5 +56,6 @@ window.accessibleAutocomplete({
       el.textContent = confirmed.label
     }
     if (confirmed && confirmed.command) confirmed.command()
-  }
+  },
+  dropdownArrow: ({ className }) => `<svg class="${className}" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>`
 })
