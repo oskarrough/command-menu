b24bca136d72cbd7b741792841f79141314488c9
Glitch (autocomplete-adventures) <none>
2019-01-14 18:53:41 +0000

    💑🕋 Checkpoint ./script.js:136798/956 ./index.html:136798/2012

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // yarn add accessible-autocomplete fuzzysort
+const {accessibleAutocomplete, fuzzysort} = window
 
 const commands = [
   {keys: '', label: 'Help', command: () => { console.log('send help plx') }},
@@ -30,32 +31,41 @@ function suggest(query, populateResults) {
   populateResults(results)
 }
 
-window.accessibleAutocomplete({
-  element: document.querySelector('#my-autocomplete-container'),
-  id: 'my-autocomplete', // To match it to the existing <label>.
-  source: suggest,
-  placeholder: 'Search for a command',
-  autoselect: true,
-  confirmOnBlur: true, // should be true for touch at least
-  showAllValues: true,
-  // displayMenu: 'overlay',
-  templates: {
-    inputValue: (val) => {
-      if (val && val.label) return val.label
-      return val
-    },
-    suggestion: (s) => {
-      if (!s) return s
-      return `${s.label} <kbd>${s.keys}</kbd>`
-    }
-  },
-  onConfirm: (confirmed) => {
-    console.log({confirmed})
-    if (confirmed) {
-      let el = document.querySelector('#my-autocomplete-confirmed')
-      el.textContent = confirmed.label
-    }
-    if (confirmed && confirmed.command) confirmed.command()
-  },
-  dropdownArrow: ({ className }) => `<svg class="${className}" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>`
-})
+class Autocomplete extends HTMLElement {
+  connectedCallback() {
+    this.enableAutocomplete()
+  }
+  enableAutocomplete() {
+    window.accessibleAutocomplete({
+      element: this,
+      id: 'my-autocomplete', // To match it to the existing <label>.
+      source: suggest,
+      placeholder: 'Search for a command',
+      autoselect: true,
+      confirmOnBlur: true, // should be true for touch at least
+      showAllValues: true,
+      // displayMenu: 'overlay',
+      templates: {
+        inputValue: (val) => {
+          if (val && val.label) return val.label
+          return val
+        },
+        suggestion: (s) => {
+          if (!s) return s
+          return `${s.label} <kbd>${s.keys}</kbd>`
+        }
+      },
+      onConfirm: (confirmed) => {
+        console.log({confirmed})
+        if (confirmed) {
+          let el = document.querySelector('#my-autocomplete-confirmed')
+          el.textContent = confirmed.label
+        }
+        if (confirmed && confirmed.command) confirmed.command()
+      },
+      dropdownArrow: ({ className }) => `<svg class="${className}" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>`
+    })
+  }
+}
+
+customElements.define('auto-complete', Autocomplete)
