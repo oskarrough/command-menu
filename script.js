316e3bee2c570da31df5de262b9eb5fa8f5a3363
Oskar Rough <oskar@rough.dk>
2023-05-26 14:41:01 +0200

    Old stuff

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import accessibleAutocomplete from 'https://cdn.skypack.dev/accessible-autocomplete'
 import fuzzysort from 'https://cdn.skypack.dev/fuzzysort';
 
-class Autocomplete extends HTMLElement {
+class CommandMenu extends HTMLElement {
   connectedCallback() {
     this.enable()
 
@@ -10,31 +10,10 @@ class Autocomplete extends HTMLElement {
       document.addEventListener('keydown', this.handleShortcut.bind(this))
     }
   }
-  // Ctrl+k or Command+k toggles it. Escape closes it, if open.
-  handleShortcut(event) {
-    if ((event.ctrlKey || event.metaKey) && event.key == "k") this.toggle()
-    if (event.key === 'Escape' && this.hasAttribute('modal')) this.close()
-  }
-  handleClick(event) {
-    if (event.target === this) {
-      this.close()
-    }
-  }
-  open() {
-    this.classList.add('is-open')
-    this.querySelector('.autocomplete__input').focus()
-    document.addEventListener('click', this.handleClick.bind(this))
-  }
-  close() {
-    this.classList.remove('is-open')
-    document.removeEventListener('click', this.handleClick.bind(this))
-  }
-  toggle() {
-    this.classList.contains('is-open') ? this.close() : this.open()
-  }
+  
   enable() {
     const list = this.list
-    if (!list) throw new Error('Could not enable command menu. Missing a list of commands')
+    if (!list) throw new Error('Could not enable command menu. Set the `list` property on the element a list of commands')
     
     accessibleAutocomplete({
       element: this,
@@ -74,6 +53,36 @@ class Autocomplete extends HTMLElement {
       dropdownArrow: ({ className }) => `<svg class="${className}" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>`
     })
   }
+
+  // Ctrl+k or Command+k toggles it. Escape closes it, if open.
+  handleShortcut(event) {
+    if ((event.ctrlKey || event.metaKey) && event.key == "k") {
+      event.preventDefault()
+      this.toggle()
+    }
+    if (event.key === 'Escape' && this.hasAttribute('modal')) this.close()
+  }
+
+  handleClick(event) {
+    if (event.target === this) {
+      this.close()
+    }
+  }
+
+  open() {
+    this.classList.add('is-open')
+    this.querySelector('.autocomplete__input').focus()
+    document.addEventListener('click', this.handleClick.bind(this))
+  }
+
+  close() {
+    this.classList.remove('is-open')
+    document.removeEventListener('click', this.handleClick.bind(this))
+  }
+
+  toggle() {
+    this.classList.contains('is-open') ? this.close() : this.open()
+  } 
 }
 
-customElements.define('command-menu', Autocomplete)
+customElements.define('command-menu', CommandMenu)
